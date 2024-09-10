095520bf5cc63c2f99de45bfdc8b5781c36b89a6
valariz <32249050+valariz@users.noreply.github.com>
2024-09-10 12:48:08 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -19,7 +19,7 @@ trap _term SIGTERM
 ####################
 # Start Matterbridge #
 ####################
-./matterbridge -conf=config/config.toml -debug -version &
+./matterbridge -conf=config/config.toml -debug &
 
 PID=$!
 
